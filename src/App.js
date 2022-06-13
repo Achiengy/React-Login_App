4949dd3c162bca5ad6e9316bde9497eb2f3cca9c
+ <+>
2022-06-13 12:34:26 +0300

    Add a function for input placeholders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+ import {signup} from "./firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ export default function App() {
+  return(
+    <div id="main">
+<input placeholder="Email" />
+<input placeholder="Password" />
     </div>
   );
-}
-
-export default App;
+ }
